ec50785fac36b39364f019d528491ba5a786bfbb
Ralph Woiwode <rawoiwode@gmail.com>
2025-09-01 14:49:39 -0500

    Adding simple gestures for EmojiSticker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ import ImageViewer from "@/components/ImageViewer";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
 import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -54,7 +55,7 @@ export default function Index() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageConatiner}>
         <ImageViewer
           imgSource={PlaceholderImage}
@@ -92,7 +93,7 @@ export default function Index() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
